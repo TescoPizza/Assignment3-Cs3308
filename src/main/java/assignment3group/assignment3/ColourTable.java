c9574e305536d934f7053fe8adb53c36b9364a79
Charles O'Brien <charlesobrien101@gmail.com>
2023-12-05 21:43:09 +0000

    Generated Java docs for code using Trelent plugin.

--- src/main/java/assignment3group/assignment3/ColourTable.java
+++ src/main/java/assignment3group/assignment3/ColourTable.java
@@ -11,6 +11,11 @@ public class ColourTable {
     private List<Integer> palette;
 
 
+    /**
+     * The ColourTable function is used to create a new ColourTable object.
+     * @param paletteSize Determine the size of the palette
+     * @docauthor Trelent
+     */
     public ColourTable(int paletteSize) {
         if (!isValidPaletteSize(paletteSize)) {
             throw new IllegalArgumentException("Invalid Palette Size");
@@ -20,6 +25,11 @@ public class ColourTable {
 
 
     }
+    /**
+     * The add function adds a colour to the ColourTable.
+     * @param colour Add a colour to the palette
+     * @docauthor Trelent
+     */
     public void add(int colour) {
         if (checkNotExceedingTableSize()){
             throw new IllegalStateException("Exceeding capacity of ColourTable");
@@ -33,20 +43,45 @@ public class ColourTable {
 
         palette.add(colour);
     }
+    /**
+     * The isValidPaletteSize function checks to see if the paletteSize is a power of 2.
+     * @param paletteSize Check if the palette size is greater than 1 and less than 1024
+     * @return True if palettesize is greater than 1, less than or equal to 1024, and a power of 2
+     * @docauthor Trelent
+     */
     private boolean isValidPaletteSize(int paletteSize) {
         return paletteSize > 1 && paletteSize <= 1024 && (paletteSize & (paletteSize - 1)) == 0;
 
     }
 
+    /**
+     * The checkNotExceedingTableSize function checks if the size of the palette is greater than or equal to
+     * the maximum number of colors allowed in a GIF image. If it is, then we return true and stop adding more
+     * colors to our palette. Otherwise, we return false and continue adding more colors to our palette.
+     * @return A boolean value
+     * @docauthor Trelent
+     */
     private boolean checkNotExceedingTableSize() {
         return this.palette.size() >= paletteSize;
 
     }
 
+    /**
+     * The checkValidRgb function checks if the colour is valid.
+     * @param colour Check if the value is between 0 and 16777215
+     * @return True if the colour is between 0 and 0xffffff (inclusive),
+     * @docauthor Trelent
+     */
     public boolean checkValidRgb(int colour) {
         return colour >= 0 && colour <= 0xFFFFFF;
 
     }
+    /**
+     * The checkIfContainsColour function checks if the colour is in the palette.
+     * @param colour Check if the colour is in the palette
+     * @return True if the palette contains a given colour, and false otherwise
+     * @docauthor Trelent
+     */
     public boolean checkIfContainsColour(int colour) {
         return this.palette.contains(colour);
     }
--- src/main/java/assignment3group/assignment3/ColourTableTest.java
+++ src/main/java/assignment3group/assignment3/ColourTableTest.java
@@ -6,23 +6,41 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ColourTableTest {
+    /**
+     * The TestValidPaletteSize function tests the ColourTable constructor to ensure that it throws an exception when
+     * a palette size is passed in that is not a power of 2.
+     * @docauthor Trelent
+     */
     @Test
     public void TestValidPaletteSize(){
         assertDoesNotThrow(() -> new ColourTable(32));
 
     }
+    /**
+     * The testInvalidPaletteSizes function tests the ColourTable constructor to ensure that it throws an IllegalArgumentException when given a palette size of 1023, - 1 or 1.
+     * @docauthor Trelent
+     */
     @Test
     public void testInvalidPaletteSizes() {
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(1023));
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(-1));
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(1));
     }
+    /**
+     * The testNotExceedingTables function tests the ColourTable class to see if it can add a value that is less than or equal to the maximum number of tables.
+     * @docauthor Trelent
+     */
     @Test
     public void testNotExceedingTables() {
         ColourTable ColourTable = new ColourTable(2);
         assertDoesNotThrow(() -> ColourTable.add(10));
 
     }
+    /**
+     * The testExceedingTables function tests the add function to see if it throws an IllegalStateException when
+     * the number of elements in the table exceeds its capacity.
+     * @docauthor Trelent
+     */
     @Test
     public void testExceedingTables() {
         ColourTable ColourTable = new ColourTable(2);
@@ -32,18 +50,33 @@ public class ColourTableTest {
 
     }
 
+    /**
+     * The testValidRgbColours function tests the add function in ColourTable.java to ensure that it can handle
+     * valid RGB colours, which are integers between 0 and 255 inclusive. The testValidRgbColours function does this by
+     * calling the assertDoesNotThrow method from JUnit 5, which checks if an exception is thrown when a given lambda expression is executed.
+     * @docauthor Trelent
+     */
     @Test
     public void testValidRgbColours(){
         ColourTable ColourTable = new ColourTable(2);
         assertDoesNotThrow(() ->  ColourTable.add(111));
         assertDoesNotThrow(() -> ColourTable.add(0x00000A));
     }
+    /**
+     * The testInValidRgbColours function tests the add function in ColourTable.java to ensure that it throws an IllegalArgumentException when given a value less than 0 or greater than 16777215 (0xFFFFFF).
+     * @docauthor Trelent
+     */
     @Test
     public void testInValidRgbColours(){
         ColourTable ColourTable = new ColourTable(2);
         assertThrows(IllegalArgumentException.class, () -> ColourTable.add(-10));
         assertThrows(IllegalArgumentException.class, () -> ColourTable.add(0x1000000));
     }
+    /**
+     * The testIfAddsColourTwice function tests if the add function adds a colour twice.
+     * It does this by creating a new ColourTable object with 2 colours, and then adding 10 to it 4 times.
+     * The assertDoesNotThrow statement checks that there is no error thrown when adding 10 to the ColourTable object, as there is only one colour in the pallete.
+     */
     @Test
     public void testIfAddsColourTwice(){
         ColourTable ColourTable = new ColourTable(2);
